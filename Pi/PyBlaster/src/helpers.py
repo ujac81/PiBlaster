cfb6340bef48aa9c80bd70c4fd7f118aaa69a2f9
ujac81 <ulrich.jansen@rwth-aachen.de>
2014-01-03 14:38:40 +0100

    1st real working bluetooth comm version

---
+++ Pi/PyBlaster/src/helpers.py
@@ -0,0 +1,17 @@
+"""helpers.py -- supporting routines for PyBlaster project
+
+@Author Ulrich Jansen <ulrich.jansen@rwth-aachen.de>
+"""
+
+
+
+suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+def humansize(nbytes):
+    if nbytes == 0: return '0 B'
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes)-1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
+
